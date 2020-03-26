264953b0c593ec219af9897b676212a7da7b6149
dan63047 <dan63047@gmail.com>
2020-03-26 17:06:23 +0300

    Updates

---
+++ precache-manifest.cf816a7a8bf0939d7b2f3720357ec799.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477160998172addceb14a108d9a38acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da27735245f138c13c9",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "5aa754fbd8a50ed9df97",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3da27735245f138c13c9",
+    "url": "/static/js/2.1814b924.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.1814b924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa754fbd8a50ed9df97",
+    "url": "/static/js/main.aa605e2a.chunk.js"
+  },
+  {
+    "revision": "b80dd97a5405b779ef51",
+    "url": "/static/js/runtime-main.44cd59d9.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
