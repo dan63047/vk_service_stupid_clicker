d7106ee698b6cc9fc04fd3bcf39bf4417c66ab64
dan63047 <dan63047@gmail.com>
2020-03-26 22:26:21 +0300

    deleting

--- src/App.js
+++ src/App.js
@@ -8,34 +8,15 @@ import Home from './panels/Home';
 import Persik from './panels/Persik';
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
-
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
-
 	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
+		<View activePanel="main">
+		<Panel id="main">
+			<PanelHeader>VKUI</PanelHeader>
+			<Group header={<Header mode="secondary">Items</Header>}>
+			<Cell>Hello</Cell>
+			<Cell>World</Cell>
+			</Group>
+		</Panel>
 		</View>
 	);
 }
